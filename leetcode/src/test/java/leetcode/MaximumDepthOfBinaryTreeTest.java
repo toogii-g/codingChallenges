1adb09bc9fef732393023c08570ace2071432d1d
bitoogii <bitoogii@yahoo.com>
2025-01-15 23:57:50 -0600

    maxDepthBinaryTree

---
+++ leetcode/src/test/java/leetcode/MaximumDepthOfBinaryTreeTest.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MaximumDepthOfBinaryTreeTest {
+
+    @Test
+    public void test1() {
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20, node4, node5);
+        TreeNode root = new TreeNode(3, node2, node3);
+
+        MaximumDepthOfBinaryTree maxDBT = new MaximumDepthOfBinaryTree();
+
+        int result = maxDBT.maxDepth(root);
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void test2() {
+        TreeNode node2 = new TreeNode(9);
+        TreeNode root = new TreeNode(3, node2, null);
+        
+        MaximumDepthOfBinaryTree maxDBT = new MaximumDepthOfBinaryTree();
+
+        int result = maxDBT.maxDepth(root);
+
+        assertEquals(2, result);
+    }
+}
