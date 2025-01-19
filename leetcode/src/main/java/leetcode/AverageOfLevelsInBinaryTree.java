5e8c44ea7ab6eaedea6544a8955f7b62c5951ce2
bitoogii <bitoogii@yahoo.com>
2025-01-18 23:26:59 -0600

    Average of Levels in Binary Tree

---
+++ leetcode/src/main/java/leetcode/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class AverageOfLevelsInBinaryTree {
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>(List.of(root));
+        List<Double> result = new ArrayList<>();
+        while(!q.isEmpty()){
+            double qLength = q.size();
+            double row = 0;
+            for(int i =0; i<qLength;i++){
+                TreeNode curr = q.poll();
+                row+= curr.val;
+                if(curr.left != null) q.offer(curr.left);
+                if(curr.right != null) q.offer(curr.right);
+            }
+            result.add(row/qLength);
+        }
+
+        return result;
+    }
+}
---
+++ leetcode/src/test/java/leetcode/AverageOfLevelsInBinaryTreeTest.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AverageOfLevelsInBinaryTreeTest {
+
+    @Test
+    public void test1() {
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20, node4, node5);
+        TreeNode root = new TreeNode(3, node2, node3);
+
+        AverageOfLevelsInBinaryTree avgLevels = new AverageOfLevelsInBinaryTree();
+
+        List<Double> result = avgLevels.averageOfLevels(root);
+        List<Double> expected = List.of(3.0d,14.5d, 11.0d);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void test2() {
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        TreeNode node2 = new TreeNode(9,node4,node5);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode root = new TreeNode(3, node2, node3);
+
+        AverageOfLevelsInBinaryTree avgLevels = new AverageOfLevelsInBinaryTree();
+
+        List<Double> result = avgLevels.averageOfLevels(root);
+        List<Double> expected = List.of(3.0d,14.5d, 11.0d);
+        assertEquals(expected, result);
+    }
+}
