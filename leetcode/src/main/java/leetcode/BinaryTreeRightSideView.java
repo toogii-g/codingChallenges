776c61cefaeacae68c4e609b221ceb9d22b1cbc0
bitoogii <bitoogii@yahoo.com>
2025-01-18 23:27:11 -0600

    Binary Tree Right Side View

---
+++ leetcode/src/main/java/leetcode/BinaryTreeRightSideView.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>(List.of(root));
+
+
+        while (!queue.isEmpty()) {
+            int qLength = queue.size();
+            for (int i = 0; i < qLength; i++) {
+                TreeNode curr = queue.poll();
+                if (curr != null && i + 1 == qLength) {
+                    result.add(curr.val);
+                }
+                if (curr != null && curr.left != null) queue.offer(curr.left);
+                if (curr != null && curr.right != null) queue.offer(curr.right);
+            }
+        }
+
+        return result;
+    }
+}
