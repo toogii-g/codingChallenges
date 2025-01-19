550494a59804afef1663ad971c2897a1b11d1cb2
bitoogii <bitoogii@yahoo.com>
2025-01-19 13:18:56 -0600

    Validate BST

---
+++ leetcode/src/main/java/leetcode/ValidateBST.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class ValidateBST {
+    TreeNode prev = null;
+    boolean ans = true;
+
+    public boolean isValidBST(TreeNode root) {
+        validateBST(root);
+        return ans;
+    }
+
+    private void validateBST(TreeNode node) {
+        if (node == null || !ans) return;
+
+        validateBST(node.left);
+        if (ans && prev != null && prev.val >= node.val) {
+            ans = false;
+        }
+        prev = node;
+        validateBST(node.right);
+    }
+}
