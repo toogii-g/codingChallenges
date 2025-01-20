e7d40006de58ef50bebfd4f6733e577406ab544f
bitoogii <bitoogii@yahoo.com>
2025-01-19 23:43:44 -0600

    PathSum

---
+++ leetcode/src/main/java/leetcode/binarytree/PathSum.java
@@ -0,0 +1,22 @@
+package leetcode.binarytree;
+
+public class PathSum {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        return checkSum(root, targetSum, 0);
+    }
+
+    private boolean checkSum(TreeNode node, int targetSum, int prevVal) {
+        if (node.left == null && node.right == null) {
+            if (prevVal + node.val == targetSum) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return (node.left != null && checkSum(node.left, targetSum, prevVal + node.val))
+                || (node.right != null && checkSum(node.right, targetSum, prevVal + node.val));
+    }
+}
---
+++ leetcode/src/test/java/leetcode/BinaryTree/PathSumTest.java
@@ -0,0 +1,52 @@
+package leetcode.BinaryTree;
+
+import leetcode.binarytree.AverageOfLevelsInBinaryTree;
+import leetcode.binarytree.PathSum;
+import leetcode.binarytree.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PathSumTest {
+
+    @Test
+    public void test1() {
+        TreeNode node9 = new TreeNode(1);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node8 = new TreeNode(2);
+        TreeNode node6 = new TreeNode(11, node7, node8);
+
+        TreeNode node4 = new TreeNode(13);
+        TreeNode node5 = new TreeNode(4, null, node9);
+        TreeNode node2 = new TreeNode(4, node6, null);
+        TreeNode node3 = new TreeNode(8, node4, node5);
+        TreeNode root = new TreeNode(5, node2, node3);
+
+        PathSum target = new PathSum();
+
+        boolean result = target.hasPathSum(root, 22);
+        assertTrue(result);
+    }
+
+    @Test
+    public void test2() {
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode root = new TreeNode(1, node2, node3);
+
+        PathSum target = new PathSum();
+
+        boolean result = target.hasPathSum(root, 5);
+        assertFalse(result);
+    }
+
+    @Test
+    public void test3() {
+        PathSum target = new PathSum();
+
+        boolean result = target.hasPathSum(null, 0);
+        assertFalse(result);
+    }
+}
