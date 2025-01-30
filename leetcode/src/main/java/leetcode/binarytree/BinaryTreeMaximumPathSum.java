872123358746f134cd10b047a5e5363b2de69da4
bitoogii <bitoogii@yahoo.com>
2025-01-29 18:23:05 -0600

    Binary Tree Maximum Path Sum

---
+++ leetcode/src/main/java/leetcode/binarytree/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,22 @@
+package leetcode.binarytree;
+
+public class BinaryTreeMaximumPathSum {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode node){
+        if(node == null) return 0;
+
+        int left = Math.max(dfs(node.left),0);
+        int right = Math.max(dfs(node.right),0);
+
+        max=Math.max(max, node.val+left+right);
+
+        return node.val + Math.max(left, right);
+    }
+
+
+}
---
+++ leetcode/src/test/java/leetcode/BinaryTree/BinaryTreeMaximumPathSumTest.java
@@ -0,0 +1,34 @@
+package leetcode.BinaryTree;
+
+import leetcode.binarytree.BinaryTreeMaximumPathSum;
+import leetcode.binarytree.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BinaryTreeMaximumPathSumTest {
+
+    @Test
+    public void test1() {
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode root = new TreeNode(1, node2, node3);
+
+        BinaryTreeMaximumPathSum target = new BinaryTreeMaximumPathSum();
+        int result = target.maxPathSum(root);
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void test2() {
+        TreeNode node5 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node3 = new TreeNode(20, node4, node5);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode root = new TreeNode(-10, node2, node3);
+
+        BinaryTreeMaximumPathSum target = new BinaryTreeMaximumPathSum();
+        int result = target.maxPathSum(root);
+        assertEquals(42, result);
+    }
+}
